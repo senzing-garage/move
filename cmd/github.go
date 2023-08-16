3724b341bd5dde7079981ab1db6bafc88e94b7a7
roncewind <dad@lynntribe.net>
2023-08-16 13:36:20 -0700

    #13 rerun git actions

--- cmd/github.go
+++ cmd/github.go
@@ -1,7 +1,9 @@
 // 0.0.4
 // Created by make-go-github-file.yaml on Mon Aug 14 19:48:41 UTC 2023
-//lint:file-ignore U1000 Ignore all unused code, it's generated
+
 package cmd
+
+//lint:file-ignore U1000 Ignore all unused code, it's generated
 
 var (
 	githubDate           string = "2023-08-14"
