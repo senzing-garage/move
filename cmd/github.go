6fb08b2d97b13fe5eec0eb8d496642972c597733
roncewind <roncewind@users.noreply.github.com>
2023-08-16 20:45:24 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.0.5

--- cmd/github.go
+++ cmd/github.go
@@ -1,16 +1,14 @@
-// 0.0.4
-// Created by make-go-github-file.yaml on Mon Aug 14 19:48:41 UTC 2023
-
+// 0.0.5
+// Created by make-go-github-file.yaml on Wed Aug 16 20:45:24 UTC 2023
+//lint:file-ignore U1000 Ignore all unused code, it's generated
 package cmd
 
-//lint:file-ignore U1000 Ignore all unused code, it's generated
-
 var (
-	githubDate           string = "2023-08-14"
+	githubDate           string = "2023-08-16"
 	githubIteration      string = "0"
-	githubRef            string = "refs/tags/0.0.4"
-	githubRefName        string = "0.0.4"
+	githubRef            string = "refs/tags/0.0.5"
+	githubRefName        string = "0.0.5"
 	githubRepository     string = "Senzing/move"
 	githubRepositoryName string = "move"
-	githubVersion        string = "0.0.4"
+	githubVersion        string = "0.0.5"
 )
