21a53762c7f4dfbfcbb6def7ddaaf84901e05a85
docktermj@users.noreply.github.com <docktermj@users.noreply.github.com@users.noreply.github.com>
2023-08-29 17:15:26 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.0.6

--- cmd/github.go
+++ cmd/github.go
@@ -1,16 +1,15 @@
-// 0.0.4
-// Created by make-go-github-file.yaml on Mon Aug 14 19:48:41 UTC 2023
-
+// 0.0.6
+// Created by make-go-github-file.yaml on Tue Aug 29 17:15:26 UTC 2023
+//
+//lint:file-ignore U1000 Ignore all unused code, it's generated
 package cmd
 
-//lint:file-ignore U1000 Ignore all unused code, it's generated
-
 var (
-	githubDate           string = "2023-08-14"
+	githubDate           string = "2023-08-29"
 	githubIteration      string = "0"
-	githubRef            string = "refs/tags/0.0.4"
-	githubRefName        string = "0.0.4"
+	githubRef            string = "refs/tags/0.0.6"
+	githubRefName        string = "0.0.6"
 	githubRepository     string = "Senzing/move"
 	githubRepositoryName string = "move"
-	githubVersion        string = "0.0.4"
+	githubVersion        string = "0.0.6"
 )
